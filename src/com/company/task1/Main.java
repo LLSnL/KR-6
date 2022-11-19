ef50888c8826c640568d884a450d554add0e7b35
Руслан Шишанов <schischanov.ruslan@yandex.ru>
2022-11-19 12:43:05 +0300

    Доработано задание 1 КР-6

--- src/com/company/task1/Main.java
+++ src/com/company/task1/Main.java
@@ -9,25 +9,26 @@ public class Main {
         System.out.println("Введите, площадь какой фигуры вы хотите посчитать:" +
                 "\n1 - Квадрат, 2 - Круг, 3 - Правильный треугольник");
         int choice = sc.nextInt();
+        Area area;
         switch (choice) {
             case 1 -> {
                 System.out.println("Введите сторону квадрата");
                 double side = scanner.nextDouble();
-                Square square = new Square(side);
-                square.calculateArea();
+                area = new Square(side);
+                area.calculateArea();
             }
             case 2 -> {
                 System.out.println("Введите радиус круга");
                 double radius = scanner.nextDouble();
-                Circle circle = new Circle(radius);
-                circle.calculateArea();
+                area = new Circle(radius);
+                area.calculateArea();
             }
             case 3 -> {
                 System.out.println("Введите 2 катета треугольника");
                 double firstC = scanner.nextDouble();
                 double secondC = scanner.nextDouble();
-                RightTriangle rightTriangle = new RightTriangle(firstC, secondC);
-                rightTriangle.calculateArea();
+                area = new RightTriangle(firstC, secondC);
+                area.calculateArea();
             }
             default -> System.out.println("Неправильный ввод!");
         }
